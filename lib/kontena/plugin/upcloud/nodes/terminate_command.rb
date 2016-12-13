4886a7e511ce1081433d6cd361d54db384cd382f
Kimmo Lehto <kimmo.lehto@kontena.io>
2016-12-13 10:41:39 +0200

    Confirm node terminate

--- lib/kontena/plugin/upcloud/nodes/terminate_command.rb
+++ lib/kontena/plugin/upcloud/nodes/terminate_command.rb
@@ -7,10 +7,13 @@ module Kontena::Plugin::Upcloud::Nodes
     option "--username", "USER", "Upcloud username", required: true
     option "--password", "PASS", "Upcloud password", required: true
 
+    option  '--force', :flag, "Force terminate", attribute_name: :forced
+
     requires_current_master_token
 
     def execute
       require_relative '../../../machine/upcloud'
+      confirm_command(name) unless forced?
       grid = client.get("grids/#{current_grid}")
       destroyer = Kontena::Machine::Upcloud::NodeDestroyer.new(client, username, password)
       destroyer.run!(grid, name)
